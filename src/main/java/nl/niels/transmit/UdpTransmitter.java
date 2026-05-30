d28dd9a384bf307c044fa23cca287d3fe2980a68
Niels Van Nerum <vannerumniels@icloud.com>
2026-05-30 14:22:30 +0200

    write UdpTransmitter, mapping the measurement to fit a single byte, and broadcasts it

--- src/main/java/nl/niels/transmit/UdpTransmitter.java
+++ src/main/java/nl/niels/transmit/UdpTransmitter.java
@@ -1,4 +1,39 @@
 package nl.niels.transmit;
 
-public class UdpTransmitter {
+import java.io.IOException;
+import java.net.*;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class UdpTransmitter implements Runnable {
+    private final LinkedBlockingQueue<double[]> consumingQueue;
+    private DatagramSocket socket;
+    private InetAddress address;
+
+    public UdpTransmitter(LinkedBlockingQueue<double[]> consumingQueue) throws SocketException {
+        this.consumingQueue = consumingQueue;
+        socket = new DatagramSocket();
+        socket.setBroadcast(true);
+        address = InetAddress.ofLiteral("192.168.1.255");
+    }
+    
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                byte[] bytes = toBytes(consumingQueue.take());
+                DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, 4445);
+                socket.send(packet);
+            }
+        } catch (InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    protected byte[] toBytes(double[] measurements) {
+        byte[] bytes = new byte[measurements.length];
+        for (int i = 0; i < measurements.length; i++) {
+            bytes[i] = (byte) (measurements[i] * 255);
+        }
+        return bytes;
+    }
 }
