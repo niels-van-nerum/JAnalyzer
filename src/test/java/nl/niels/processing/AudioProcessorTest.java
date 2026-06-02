214f75fe9e3859c7450a21b2ffde70190568257d
Niels Van Nerum <vannerumniels@icloud.com>
2026-06-02 17:09:24 +0200

    Write test verifying functionality of runFft

--- src/test/java/nl/niels/processing/AudioProcessorTest.java
+++ src/test/java/nl/niels/processing/AudioProcessorTest.java
@@ -1,5 +1,6 @@
 package nl.niels.processing;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -17,5 +18,45 @@ public class AudioProcessorTest {
         double[] result = processor.toSamples(chunk);
 
         assertArrayEquals(expectedResult, result, 0.0001);
+    }
+
+    @Test
+    void runFft_440HzSignal_returns440HzAsMostSignificantBin() {
+        double[] chunk = new double[1024];
+        for (int i = 0; i < chunk.length; i++) {
+            chunk[i] = Math.sin(2 * Math.PI * 440 * i / 44100);
+        }
+
+        AudioProcessor processor = new AudioProcessor(null, null);
+        double[] fft = processor.runFft(chunk);
+
+        int bin = 440 * 1024 / 44100;
+
+        double[] magnitudes = new double[fft.length / 4];
+        double max = 0;
+        for (int i = 0; i < (fft.length / 4); i++) {
+            magnitudes[i] = Math.sqrt(Math.pow(fft[i * 2], 2) + Math.pow(fft[i * 2 + 1], 2));
+            if (magnitudes[i] > max) {
+                max = magnitudes[i];
+            }
+        }
+
+        Assertions.assertEquals(magnitudes[bin], max);
+    }
+
+    @Test
+    void toMagnitudes() {
+    }
+
+    @Test
+    void toBuckets() {
+    }
+
+    @Test
+    void normalize() {
+    }
+
+    @Test
+    void addHistory() {
     }
 }
